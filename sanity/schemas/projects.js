9d461cc309eff0566db8c5a69f8e668e6f9d246e
Robert Leu <robertleu@gmail.com>
2020-12-29 14:41:58 -0800

    First sanity schema

---
+++ sanity/schemas/projects.js
@@ -0,0 +1,15 @@
+export default {
+    // Computer Name
+    name: 'projects',
+    // visible title
+    title: 'Projects',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Project Name',
+            type: 'string',
+            description: 'Name of the Project',
+        }
+    ]
+}
